bd9f619b99dba2a9d9b4f72a23d31257dd869897
Simon Degen <mil-sideg@fiskeridir.no>
2023-04-14 14:59:54 +0200

    first push

---
+++ src/components/hand.jsx
@@ -0,0 +1,27 @@
+
+const Hand = (prop) => {
+  const [hand, setHand] = useState({});
+
+  function getHand() {
+
+  }
+
+  function playCard() {
+
+  }
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex
+`
+
+
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Hand;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background: red;
+  width: 100px;
+  height: 100px;
+`;
+const List
+
+export const HomePage = (props) => {
+  return <Wrapper>testing </Wrapper>;
+};
