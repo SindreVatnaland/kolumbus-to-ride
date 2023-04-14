531833de735aec1856789b9aa83500526d437d99
Simon Degen <mil-sideg@fiskeridir.no>
2023-04-14 15:40:51 +0200

    added layout

--- src/components/hand.jsx
+++ src/components/hand.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
+import styled from "styled-components";
 
 const Hand = (prop) => {
-  const [hand, setHand] = useState({});
+  const [cards, setCards] = useState([{}, {}, {}, {}, {}]);
 
   function getHand() {
-
+    console.log("draw hand")
   }
 
-  function playCard() {
+  function playCard(cardId) {
+    console.log(cardId)
 
+    if (!cards.length) getHand();
   }
 const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  flex
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
 `
 
+const Card = styled.div`
+  width: 3rem;
+  height: 5rem;
+  padding: 2em;
+  background-image: url(${props => props.imageUrl});
+  background-size: cover;
+  z-index: 1000;
+  overflow: visible;
+`
 
   return (
     <Container>
-      
+      {
+        cards.map(card => 
+          <Card imageUrl="https://deckofcardsapi.com/static/img/6H.png" alt="" onClick={() => {playCard(card?.id ?? 0)}} />
+        )
+      }
     </Container>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,71 @@
 import React from "react";
 import styled from "styled-components";
+import Hand from "../components/hand";
 
 const Wrapper = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
   background: red;
-  width: 100px;
-  height: 100px;
+  width: 100vw;
+  height: 100vh;
+  padding: 1rem;
+  box-sizing: border-box;
 `;
-const List
+const HorizontalWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  background-color: blue;
+  max-height: 60%;
+  gap: 1rem;
+`;
+
+const LocationList = styled.div`
+  position: relative;
+  width: 60%;
+  height: 100%;
+  background: yellow;
+`;
+const ScoreWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Score = styled.div`
+  display: flex;
+  height: 50%;
+  width: 100%;
+  background: green;
+`;
+
+const FrameWrapper = styled.iframe`
+  width: 100%;
+  height: 150%;
+  transform: scale(0.4);
+`;
 
 export const HomePage = (props) => {
-  return <Wrapper>testing </Wrapper>;
+  return (
+    <Wrapper>
+      <HorizontalWrapper>
+        <LocationList>
+        </LocationList>
+        <ScoreWrapper>
+          <Score>
+          <h1>Totalt tid brukt er: 10:30</h1>
+          </Score>
+          <Score>
+            <h2>Du er på holdeplass: </h2>
+            <h2>Madla</h2>
+          </Score>
+        </ScoreWrapper>
+      </HorizontalWrapper>
+      <HorizontalWrapper>
+        <Hand />
+      </HorizontalWrapper>
+    </Wrapper>
+  );
 };
